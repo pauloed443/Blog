fa131e6823e5c5d3b4c59502e565dbc9d44d460c
pauloed443 <pauloed443@gmail.com>
2019-06-14 14:23:01 -0300

    Criação do componente Painel
    Componente com o mesmo componente dentro

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,43 @@
 @extends('layouts.app')
 
 @section('content')
-<example-component></example-component>
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-10">
+                <painel titulo="Dashboard">
+                    Painel para o Dashboard.
+                    <div class="row">
+                        <div class="col-md-4">
+                            <painel titulo="Conteúdo 1">
+                                Painel para o 1º conteúdo.
+                            </painel>
+                        </div>
+                        
+                        <div class="col-md-4">
+                            <painel titulo="Conteúdo 2">
+                                Painel para o 2º conteúdo.
+                            </painel>
+                        </div>
+                        
+                        <div class="col-md-4">
+                            <painel titulo="Conteúdo 3">
+                                Painel para o 3º conteúdo.
+                            </painel>
+                        </div>
+                    </div>
+                </painel>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
+
 @endsection
