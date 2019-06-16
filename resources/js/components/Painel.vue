47961a686152bf3d5810cb7974d27b662fd10a9c
pauloed443 <pauloed443@gmail.com>
2019-06-16 03:48:08 -0300

    Estilização personalizada com CSS

--- resources/js/components/Painel.vue
+++ resources/js/components/Painel.vue
@@ -1,8 +1,10 @@
 <template>
     <div class="card">
-        <div class="card-header">{{titulo}}</div>
+        <div v-bind:class="defineCorTitulo">
+            {{titulo}}
+        </div>
 
-        <div class="card-body">
+        <div v-bind:class="defineCorTituloBody">
             <slot></slot>
         </div>
     </div>
@@ -10,6 +12,46 @@
 
 <script>
     export default {
-        props: ['titulo']
+        props: ['titulo', 'cortitulo', 'cortextotitulo', 'cortitulobody'],
+        computed: {
+            defineCorTitulo: function(){
+                return "card-header " + (this.cortextotitulo || "") + " " + (this.cortitulo || "");
+            },
+            defineCorTituloBody: function(){
+                return "card-body " + (this.cortitulobody || "");
+            }
+        }
     }
 </script>
+
+<style>
+
+.card > .blue {
+    padding: 0.75rem 1.25rem;
+    margin-bottom: 0;
+    background-color: #0f35a0 !important;
+    color: #ffffff;
+    border-bottom: 1px solid rgba(0, 0, 0, 0.125);
+}
+
+.card > .blue-body {
+    flex: 1 1 auto;
+    padding: 1.25rem;
+    border: solid #0f35a0 1px;
+}
+
+.card > .orange {
+    padding: 0.75rem 1.25rem;
+    margin-bottom: 0;
+    background-color: #ef5f0a !important;
+    color: #ffffff;
+    border-bottom: 1px solid rgba(0, 0, 0, 0.125);
+}
+
+.card > .orange-body {
+    flex: 1 1 auto;
+    padding: 1.25rem;
+    border: solid #ef5f0a 1px;
+}
+
+</style>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,43 +1,33 @@
 @extends('layouts.app')
 
 @section('content')
-<template>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-10">
-                <painel titulo="Dashboard">
-                    Painel para o Dashboard.
-                    <div class="row">
-                        <div class="col-md-4">
-                            <painel titulo="Conteúdo 1">
-                                Painel para o 1º conteúdo.
-                            </painel>
-                        </div>
-                        
-                        <div class="col-md-4">
-                            <painel titulo="Conteúdo 2">
-                                Painel para o 2º conteúdo.
-                            </painel>
-                        </div>
-                        
-                        <div class="col-md-4">
-                            <painel titulo="Conteúdo 3">
-                                Painel para o 3º conteúdo.
-                            </painel>
-                        </div>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-10">
+            <painel titulo="Dashboard">
+                Painel para o Dashboard.
+                <div class="row">
+                    <div class="col-md-4">
+                        <painel titulo="Conteúdo 1" cortitulo="blue" cortitulobody="blue-body">
+                            Painel para o 1º conteúdo.
+                        </painel>
                     </div>
-                </painel>
-            </div>
+                    
+                    <div class="col-md-4">
+                        <painel titulo="Conteúdo 2">
+                            Painel para o 2º conteúdo.
+                        </painel>
+                    </div>
+                    
+                    <div class="col-md-4">
+                        <painel titulo="Conteúdo 3" cortitulo="orange" cortitulobody="orange-body">
+                            Painel para o 3º conteúdo.
+                        </painel>
+                    </div>
+                </div>
+            </painel>
         </div>
     </div>
-</template>
-
-<script>
-    export default {
-        mounted() {
-            console.log('Component mounted.')
-        }
-    }
-</script>
+</div>
 
 @endsection
