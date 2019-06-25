e890b1c92dcaa96877438ebf9d057a0d8403fdbe
pauloed443 <pauloed443@gmail.com>
2019-06-25 08:06:55 -0300

    Componente Pagina
    componente para pagina

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,27 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-10">
-            <painel titulo="Dashboard">
-                Painel para o Dashboard.
-                <div class="row">
-                    <div class="col-md-4">
-                        <caixa qtd="781" titulo="Artigos" url="#" cor="orange" icone="ion ion-pie-graph"></caixa>
-                    </div>
-                    
-                    <div class="col-md-4">
-                        <caixa qtd="2693" titulo="Usuários" url="#" cor="green" icone="ion ion-person-stalker"></caixa>
-                    </div>
-                    
-                    <div class="col-md-4">
-                        <caixa qtd="71" titulo="Autores" url="#" cor="red" icone="ion ion-ios-people"></caixa>
-                    </div>
-                </div>
-            </painel>
+<pagina tamanho="7">
+    <painel titulo="Dashboard">
+        Painel para o Dashboard.
+        <div class="row">
+            <div class="col-md-4">
+                <caixa qtd="781" titulo="Artigos" url="#" cor="orange" icone="ion ion-pie-graph"></caixa>
+            </div>
+            
+            <div class="col-md-4">
+                <caixa qtd="2693" titulo="Usuários" url="#" cor="green" icone="ion ion-person-stalker"></caixa>
+            </div>
+            
+            <div class="col-md-4">
+                <caixa qtd="71" titulo="Autores" url="#" cor="red" icone="ion ion-ios-people"></caixa>
+            </div>
         </div>
-    </div>
-</div>
+    </painel>
+</pagina>
 
 @endsection
