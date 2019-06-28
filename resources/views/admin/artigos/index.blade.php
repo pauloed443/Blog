9b91b11a54680398fd6a69b74a5b937c3a66fba3
pauloed443 <pauloed443@gmail.com>
2019-06-28 13:40:11 -0300

    Tabela de Lista de Artigos

--- resources/views/admin/artigos/index.blade.php
+++ resources/views/admin/artigos/index.blade.php
@@ -5,6 +5,75 @@
     <painel titulo="Lista de Artigos">
         Painel para o Dashboard.
         
+        <table class="table table-striped table-hover">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Título</th>
+                    <th>Descrição</th>
+                    <th>Autor</th>
+                    <th>Data</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Título ..</td>
+                    <td>Descrição ..</td>
+                    <td>Autor ..</td>
+                    <td>Data ..</td>
+                    <td>
+                        <a href="#">Editar</a> | 
+                        <a href="#">Deletar</a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>Título ..</td>
+                    <td>Descrição ..</td>
+                    <td>Autor ..</td>
+                    <td>Data ..</td>
+                    <td>
+                        <a href="#">Editar</a> | 
+                        <a href="#">Deletar</a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>3</td>
+                    <td>Título ..</td>
+                    <td>Descrição ..</td>
+                    <td>Autor ..</td>
+                    <td>Data ..</td>
+                    <td>
+                        <a href="#">Editar</a> | 
+                        <a href="#">Deletar</a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>4</td>
+                    <td>Título ..</td>
+                    <td>Descrição ..</td>
+                    <td>Autor ..</td>
+                    <td>Data ..</td>
+                    <td>
+                        <a href="#">Editar</a> | 
+                        <a href="#">Deletar</a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>5</td>
+                    <td>Título ..</td>
+                    <td>Descrição ..</td>
+                    <td>Autor ..</td>
+                    <td>Data ..</td>
+                    <td>
+                        <a href="#">Editar</a> | 
+                        <a href="#">Deletar</a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
     </painel>
 </pagina>
 
