adc5509a5cfbb316af78738b53e326f61d8bf6cd
Paulo <pauloed443@gmail.com>
2019-07-04 23:38:23 -0300

    Componente TabelaLista (Titulos e Itens)
    Componente para listagem de itens de uma consulta, podendo especificar os prováveis títulos e a lista de itens

--- resources/views/admin/artigos/index.blade.php
+++ resources/views/admin/artigos/index.blade.php
@@ -3,77 +3,11 @@
 @section('content')
 <pagina tamanho="10">
     <painel titulo="Lista de Artigos">
-        Painel para o Dashboard.
+        <tabela-lista 
+        v-bind:titulos="['#', 'Título', 'Descrição']"
+        v-bind:itens="[[1, 'PHP O.O', 'Cuso de PHP O.O'], [1, 'VUE JS', 'Cuso de VUE JS']]"
         
-        <table class="table table-striped table-hover">
-            <thead>
-                <tr>
-                    <th>#</th>
-                    <th>Título</th>
-                    <th>Descrição</th>
-                    <th>Autor</th>
-                    <th>Data</th>
-                    <th></th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>1</td>
-                    <td>Título ..</td>
-                    <td>Descrição ..</td>
-                    <td>Autor ..</td>
-                    <td>Data ..</td>
-                    <td>
-                        <a href="#">Editar</a> | 
-                        <a href="#">Deletar</a>
-                    </td>
-                </tr>
-                <tr>
-                    <td>2</td>
-                    <td>Título ..</td>
-                    <td>Descrição ..</td>
-                    <td>Autor ..</td>
-                    <td>Data ..</td>
-                    <td>
-                        <a href="#">Editar</a> | 
-                        <a href="#">Deletar</a>
-                    </td>
-                </tr>
-                <tr>
-                    <td>3</td>
-                    <td>Título ..</td>
-                    <td>Descrição ..</td>
-                    <td>Autor ..</td>
-                    <td>Data ..</td>
-                    <td>
-                        <a href="#">Editar</a> | 
-                        <a href="#">Deletar</a>
-                    </td>
-                </tr>
-                <tr>
-                    <td>4</td>
-                    <td>Título ..</td>
-                    <td>Descrição ..</td>
-                    <td>Autor ..</td>
-                    <td>Data ..</td>
-                    <td>
-                        <a href="#">Editar</a> | 
-                        <a href="#">Deletar</a>
-                    </td>
-                </tr>
-                <tr>
-                    <td>5</td>
-                    <td>Título ..</td>
-                    <td>Descrição ..</td>
-                    <td>Autor ..</td>
-                    <td>Data ..</td>
-                    <td>
-                        <a href="#">Editar</a> | 
-                        <a href="#">Deletar</a>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
+        ></tabela-lista>
     </painel>
 </pagina>
 
