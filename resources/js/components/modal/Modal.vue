728674721dd10b243715f14d7501be656d32ae92
pauloed443 <pauloed443@gmail.com>
2019-11-22 17:12:32 -0300

    Modificando o Modal sem a Logica da busca

--- resources/js/components/modal/Modal.vue
+++ resources/js/components/modal/Modal.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="modal fade" v-bind:id="nome" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal fade" v-bind:id="nome" tabindex="-1" role="dialog" v-bind:aria-labelledby="nome" aria-hidden="true">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
             <div class="modal-header">
--- resources/views/admin/artigos/index.blade.php
+++ resources/views/admin/artigos/index.blade.php
@@ -44,7 +44,7 @@
             <button class="btn btn-primary">Salvar</button>
         </formulario>
     </modal>
-    <modal nome="detalhe" titulo="Titulo Modal Detalhe">
+    <modal nome="detalhe" titulo="Detalhes do Artigo">
         <painel v-bind:titulo="$store.state.item.titulo">
             @{{$store.state.item.descricao}}
         </painel>
